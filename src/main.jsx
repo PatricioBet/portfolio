67585cbed6701aa421e21800240c9afb93ff05b4
PatricioBet <patobetan8@gmail.com>
2025-08-08 18:43:54 -0500

    feat: integrate Vercel Analytics into the application

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './i18n.js';
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  <Analytics />
 );
